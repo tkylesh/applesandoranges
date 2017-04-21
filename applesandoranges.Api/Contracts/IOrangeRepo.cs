d0cb5f1892e5abe6331036d59e432ae7d3d63919
TRH\rhd5538 <tshuff@fbhealthplans.com>
2017-04-21 09:10:10 -0500

    repository interfaces added

---
+++ applesandoranges.Api/Contracts/IOrangeRepo.cs
@@ -0,0 +1,19 @@
+﻿using applesandoranges.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace applesandoranges.Api.Contracts
+{
+    interface IOrangeRepo
+    {
+        //get one by id
+        Orange GetOrangeById(int id);
+        //get all
+        List<Orange> GetOranges();
+        //create one
+        bool CreateOrange();
+    }
+}
